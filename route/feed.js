2e00cb746cd75ff8ba3d3ae851a88c44c9268084
fredysomy <fredysomy@gmail.com>
2020-09-14 01:32:59 +0530

    added ui

--- route/feed.js
+++ route/feed.js
@@ -17,8 +17,33 @@ app.set('views',path.join("views"));
 let usersch=require('../models/user.model');
 let tksc=require('../models/token.model');
 let blogsc = require('../models/blog.model');
+let cms=require('../models/comment.model')
 rout.route('/:id').get((req,res)=>{
-    blogsc.findById(req.params.id).then(data=> res.render('blgview',{b:md.render(data.blog)}));
+    if(req.session.user){
+    blogsc.findById(req.params.id).then(data=> {res.render('blgview',{b:md.render(data.blog),c:"<button>ADD POST</button>",obid:req.params.id})
+    cms.find({blgid:req.params.id}).then(data=>{console.log(data)}).catch(err=>{console.log("err")})}
+    );
+    
+}
+    else{
+        blogsc.findById(req.params.id).then(data=> res.render('blgview',{b:md.render(data.blog),c:'<a href="/">SIGNIN TO COMMENT</a>',obid:'_'}));
+    }
+});
+
+rout.route('/addcomment').post((req,res)=>{
+    const d=new cms();
+    d.blgid=req.body.obid;
+    d.comment=req.body.name;
+    d.comentator=req.session.user.realname;
+    d.save((err,docs)=>{
+        if(docs){
+            console.log("sucess")
+            res.redirect('back')
+        }
+        else{
+            console.log("failure")
+        }
+    })
 });
 
 
