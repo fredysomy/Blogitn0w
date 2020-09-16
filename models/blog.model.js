0f651ab7d3475dfb58351ed61aa65fd09f498b60
dude-hub <61654070+dude-hub@users.noreply.github.com>
2020-09-16 23:54:59 +0530

    Update blog.model.js

--- models/blog.model.js
+++ models/blog.model.js
@@ -4,6 +4,8 @@ var sc2= mon.Schema;
 
 var blogschema=new sc2({
     mailuser: { type: String, required: true },
+    name:{type:String,required:true},
+    uname:{type:String,required:true},
     head: { type: String, required: true },
     blog: { type: String, required: true },
     daten:{type: Date}
